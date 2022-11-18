acb36e609568fa6449fd62230dc002ea6ca79996
Andronicus Lepcha <androlepcha843@gmail.com>
2022-11-18 20:57:24 +0530

    Kan-Van

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kan-van';
+  title = 'project2';
+  one:any=["one","two","three"]
+  two:any=["one","two","three"]
+  three:any=["one","two","three"]
+  text=""
+  addText=()=>{
+    this.one.push(this.text)
+  }
+  toTwo=(one:any)=>{
+    this.one=this.one.filter((text:any)=>{
+      return text!==one
+    })
+    this.two.push(one)
+  }
+  toThree=(one:any)=>{
+    this.two=this.two.filter((text:any)=>{
+      return text!==one
+    })
+    this.three.push(one)
+  }
 }
